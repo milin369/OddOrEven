b4f81247ca56afb7da86c02ce5d2ab20b0cd8691
Milin dharmaputhran <milindharman@gmail.com>
2024-09-05 23:23:05 +0530

    first commi

---
+++ script.js
@@ -0,0 +1,13 @@
+function OddOrEven (number){
+    if(number % 2===0){
+        console.log("EVEN")
+    }else{
+        console.log("ODD")
+    }
+}
+
+OddOrEven(364)
+OddOrEven(333)
+OddOrEven(489)
+OddOrEven(67891)
+OddOrEven(10)
